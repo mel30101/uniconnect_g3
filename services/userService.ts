19b2f80761c6d0113ca1730e26ccdb5ffe78e1fc
MARIA FERNANDA HERNANDEZ OSORIO <maria.hernandez28361@ucaldas.edu.co>
2026-02-22 21:33:13 -0500

    actualización de userService para que utilice el endpoint

--- services/userService.ts
+++ services/userService.ts
@@ -1,24 +1,21 @@
-import { User } from "../types/User";
 
-const mockUsers: User[] = [
-    {
-        id: "1",
-        name: "Mafw",
-        carrera: "Ingeniería de sistemas",
-        materiasIds: ["calculo1"],
-        habilidadesIds: ["java"],
-        esMonitor: true,
-    },
-    {
-    id: "2",
-    name: "Laura Gómez",
-    carrera: "Ingeniería Industrial",
-    materiasIds: ["fisica1"],
-    habilidadesIds: ["excel"],
-    esMonitor: false,
-  },
-];
+export const searchStudents = async (
+    name?: string,
+    subjectId?: string,
+    isMonitor?: boolean
+) => {
+    const params = new URLSearchParams();
 
-export const getUsers = async (): Promise<User[]> => {
-    return mockUsers;
+    if (name) params.append("name", name);
+    if (subjectId) params.append("subjectId", subjectId);
+    if (isMonitor) params.append("isMonitor", "true");
+
+    const response = await fetch(
+        `${process.env.EXPO_PUBLIC_BACKEND_URL}/api/search-students?${params.toString()}`
+    );
+
+    if (!response.ok) {
+        throw new Error("Error al buscar estudiante");
+    }
+    return await response.json();
 };
